10f2dd6e9b0a1a58b49b792a0ae8b20b5c753b76
prashant9912 <prashantg9912@gmail.com>
2019-09-15 04:33:33 +0530

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LottieSplashScreen } from '@ionic-native/lottie-splash-screen/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -13,15 +15,22 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private lottieSplashScreen: LottieSplashScreen
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+
+      setTimeout(() => {
+        this.lottieSplashScreen.hide()
+      }, 4000);
+    
+
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.splashScreen.hide();  // WE DONE NEED THIS 
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { LottieSplashScreen } from '@ionic-native/lottie-splash-screen/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen,LottieSplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
